793c5e97a20330368a558c839c38c07360e58334
Code4All <75253091+code4allrgpv@users.noreply.github.com>
2021-06-14 23:21:26 +0530

    Create codaemon-and-dance-pairs.cpp

---
+++ codaemon-and-dance-pairs.cpp
@@ -0,0 +1,34 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll int ans(ll int a, ll int b)
+{
+    ll int ans = 0;
+
+     
+    for (ll int i = 1; i <= a; i++) {
+       
+        ans += b / 5;
+
+        
+       
+        if(i%5 + b%5 >=5)
+            ans += 1;
+        else{}
+    }
+
+    
+    return ans;
+}
+
+ 
+int main()
+{
+   ll int a,b;
+    cin >> a >> b;
+    cout << ans(a,b);
+    return 0;
+}
+
